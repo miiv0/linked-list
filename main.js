82ee42edd3610c133940f2fb5772c6bd29c8a1a4
miiv0 <minzana2002@gmail.com>
2026-07-13 21:39:09 -0400

    structure

--- main.js
+++ main.js
@@ -1,6 +1,57 @@
 const node = null
 const nextNode = null
 
-function linkedList() {
+const value = "hello"
+const index = "2"
+
+const list = []
+
+
+function append(value) {
 
 }
+
+function prepend(value) {
+
+}
+
+function size() {
+
+}
+
+function head() {
+
+}
+
+function tail() {
+
+}
+
+function at(index) {
+
+}
+
+function pop() {
+
+}
+
+function contains(value) {
+
+}
+
+function findIndex(value) {
+
+}
+
+function toString() {
+
+}
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(list)
