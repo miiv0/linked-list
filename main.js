86501119f3610e947fc777007c7dc40f208c5556
miiv0 <minzana2002@gmail.com>
2026-07-13 21:42:04 -0400

    change

--- main.js
+++ main.js
@@ -1,51 +1,16 @@
-const node = null
-const nextNode = null
-
 const value = "hello"
 const index = "2"
-
 const list = []
 
-
-function append(value) {
-
+class node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
 }
 
-function prepend(value) {
 
-}
 
-function size() {
-
-}
-
-function head() {
-
-}
-
-function tail() {
-
-}
-
-function at(index) {
-
-}
-
-function pop() {
-
-}
-
-function contains(value) {
-
-}
-
-function findIndex(value) {
-
-}
-
-function toString() {
-
-}
 
 list.append("dog");
 list.append("cat");
@@ -53,5 +18,3 @@ list.append("parrot");
 list.append("hamster");
 list.append("snake");
 list.append("turtle");
-
-console.log(list)
