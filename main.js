5bf152763dfe26e4f7e6de499c7cbddfcd2cf13c
miiv0 <minzana2002@gmail.com>
2026-07-14 21:43:30 -0400

    add append function to linked list

--- main.js
+++ main.js
@@ -21,10 +21,15 @@ class LinkedList {
         if (this.head === null) {
             this.head = newNode;
         } else {
+            let current = this.head;
 
+            while (current.next !== null) {
+                current = current.next;
+            }
+            current.next = newNode;
         }
-
         this.size++;
+
     }
 }
 
